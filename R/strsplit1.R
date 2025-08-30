bb285dac1744bb53f84a03406fee3900968b60e1
iali-0 <isseali428@gmail.com>
2025-08-30 12:49:32 +0100

    Add documentation for strsplit1

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' Split a string
+#'
+#' @param x character vector with one element
+#' @param split what to split on
+#'
+#' @returns a character vector
+#' @export
+#'
+#' @examples
+#' x <- "alpha,beta,charlie,delta"
+#' strsplit1(x, ",")
 strsplit1 <- function(x,split){
 
   strsplit(x,split = split)[[1]]
